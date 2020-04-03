358b6d04334e54923000b3e34efc4e4b1bc30a49
pradiptodg <pradiptouwii1@gmail.com>
2020-04-03 20:38:23 +0530

    Completed extra challenges for linked list merge sort and reversal methods

--- src/test/java/com/pdg/singlylinkedlist/extrachallenges/AdvancedLinkedListTest.java
+++ src/test/java/com/pdg/singlylinkedlist/extrachallenges/AdvancedLinkedListTest.java
@@ -42,7 +42,40 @@ class AdvancedLinkedListTest {
     }
 
     @Test
-    void traverse() {
-
+    void testMergeSortForEmpty() {
+        linkedList.mergeSort();
+        assertEquals(linkedList.toString(), "HEAD -> null");
     }
+
+    @Test
+    void testMerSortForSingle() {
+        linkedList.addToFront(3);
+        linkedList.mergeSort();
+        assertEquals(linkedList.toString(), "HEAD -> 3 -> null");
+    }
+
+    @Test
+    void testMergeSortForEven() {
+        linkedList.addToFront(3);
+        linkedList.addToFront(2);
+        linkedList.addToFront(7);
+        linkedList.addToFront(10);
+        linkedList.addToFront(1);
+        linkedList.addToFront(5);
+        linkedList.mergeSort();
+        assertEquals(linkedList.toString(), "HEAD -> 1 -> 2 -> 3 -> 5 -> 7 -> 10 -> null");
+    }
+
+    @Test
+    void testMergeSortForOdd() {
+        linkedList.addToFront(3);
+        linkedList.addToFront(2);
+        linkedList.addToFront(7);
+        linkedList.addToFront(10);
+        linkedList.addToFront(1);
+        linkedList.mergeSort();
+        assertEquals(linkedList.toString(), "HEAD -> 1 -> 2 -> 3 -> 7 -> 10 -> null");
+    }
+
+
 }
